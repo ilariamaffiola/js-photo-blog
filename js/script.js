bf97fd5d1d5bab7de39baad2169367ec1e442b2d
Ilaria <maffiolailaria1@gmail.com>
2025-04-07 19:35:34 +0200

    [UPD]Add functionality to toggle overlay visibility on card click

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 // const descriptionCard = document.getElementById('description-card');
 //quindi andiamo a selezionare la row 
 const row = document.getElementById('row');
-console.log(row);
+//console.log(row);
 //e andiamo ad appendere le card create col ciclo for
 //quindi andiamo a fare una chiamata ad axios per ottenere i dati delle immagini
 axios.get('https://lanciweb.github.io/demo/api/pictures/').then((resp)=>{
-    console.log(resp.data);
+    //console.log(resp.data);
     //come vediamo qui abbiamo un array di oggetti, quindi andiamo a fare un ciclo for per creare le card
     // for (let i = 0; i < resp.data.length; i++){
     //     //visualizziamo in console i singoli oggetti dell'array così possiamo avere un idea più rapida sul da farsi
@@ -47,8 +47,25 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/').then((resp)=>{
         }
         //quindi andiamo a selezionare la row e andiamo ad appendere le card create nel ciclo for
         row.innerHTML = colls;
-        console.log(colls);
+        //console.log(colls);
     }
     renderCard(resp.data);
-});
+
 //in questo modo abbiamo creato le card in js e non in html, quindi possiamo anche decidere di fare un numero di card diverso da quelle che abbiamo in html, quindi se un giorno cambiasse il numero di elementi nell'array di oggetti ottenuto possiamo farlo senza problemi, perché tanto nel ciclo for metteremo la lunghezza dell'array di oggetti e non un numero specifico.
+
+//facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+let cards = document.querySelectorAll('.card');
+let overlay = document.querySelector('.overlay');
+console.log(overlay);
+cards.forEach(element => {
+    console.log(element);
+    element.addEventListener('click', function(){
+        if(overlay.classList.contains('d-none')){
+            overlay.classList.remove('d-none');
+        }
+        else{
+            overlay.classList.add('d-none');
+        }
+});
+});
+});
